a57a648cb085effd93cf1dc341a84362fecba370
Giulio Moro <giuliomoro@yahoo.it>
2020-04-21 09:53:59 +0000

    using math_neon for faster sine computation

--- Sine.cpp
+++ Sine.cpp
@@ -23,7 +23,7 @@ The Bela software is distributed under the GNU Lesser General Public License
 
 // Sine.cpp: file for implementing the sine oscillator class
 
-#include <cmath>
+#include <math_neon.h>
 #include "Sine.h"
 
 // Default constructor: call the specific constructor with a default value
@@ -56,7 +56,7 @@ float Sine::getFrequency() {
 // Get the next sample and update the phase
 float Sine::nextSample() {
 	// Increment and wrap the phase
-	float out = sinf(phase_);
+	float out = sinf_neon(phase_);
 	phase_ += 2.f * (float)M_PI * frequency_ / sampleRate_;
 	while(phase_ >= (float)M_PI)
 		phase_ -= 2.f * (float)M_PI;
